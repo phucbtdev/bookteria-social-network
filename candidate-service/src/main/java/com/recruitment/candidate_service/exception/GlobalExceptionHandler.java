a223540e0a63ef2d5ea5f6fbb97aab6037dc9186
Phúc Dev <phucbuitrong.dev@gmail.com>
2025-05-08 08:01:33 +0700

    Fix warning and update GlobalExceptionHandler

--- candidate-service/src/main/java/com/recruitment/candidate_service/exception/GlobalExceptionHandler.java
+++ candidate-service/src/main/java/com/recruitment/candidate_service/exception/GlobalExceptionHandler.java
@@ -1,53 +1,36 @@
 package com.recruitment.candidate_service.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.recruitment.candidate_service.dto.response.ApiResponse;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.HashMap;
-import java.util.Map;
+
+import lombok.extern.slf4j.Slf4j;
 
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
-
-    @ExceptionHandler(value = RuntimeException.class)
-    ResponseEntity<ApiResponse> handlingRuntimeException(RuntimeException exception) {
+    @ExceptionHandler(value = Exception.class)
+    ResponseEntity<ApiResponse<Object>> handlingRuntimeException(RuntimeException exception) {
         log.error("Exception: ", exception);
 
-        ApiResponse apiResponse = new ApiResponse();
-        apiResponse.setCode(ErrorCode.UNCATEGORIZED_EXCEPTION.getCode());
-        apiResponse.setMessage(ErrorCode.UNCATEGORIZED_EXCEPTION.getMessage());
-
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiResponse);
-    }
-
-    @ExceptionHandler(value = AppException.class)
-    ResponseEntity<ApiResponse> handlingAppException(AppException exception) {
-        ErrorCode errorCode = exception.getErrorCode();
-        ApiResponse apiResponse = new ApiResponse();
-
-        apiResponse.setCode(errorCode.getCode());
-        apiResponse.setMessage(errorCode.getMessage());
-
-        return ResponseEntity.status(errorCode.getStatusCode()).body(apiResponse);
-    }
-
-    @ExceptionHandler(value = AccessDeniedException.class)
-    ResponseEntity<ApiResponse> handlingAccessDeniedException(AccessDeniedException exception) {
-        ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
-
-        return ResponseEntity.status(errorCode.getStatusCode())
-                .body(ApiResponse.builder()
-                        .code(errorCode.getCode())
-                        .message(errorCode.getMessage())
-                        .build());
+        return ResponseEntity.badRequest()
+                .body(
+                        ApiResponse.builder()
+                                .code(ErrorCode.UNCATEGORIZED_EXCEPTION.getCode())
+                                .message(ErrorCode.UNCATEGORIZED_EXCEPTION.getMessage())
+                                .result(
+                                        null
+                                )
+                                .build()
+                );
     }
 
     @ExceptionHandler(value =  MethodArgumentNotValidException.class)
@@ -61,5 +44,28 @@ public class GlobalExceptionHandler {
                                 .result(errors)
                                 .build()
                 );
+    }
+
+    @ExceptionHandler(value = AppException.class)
+    ResponseEntity<ApiResponse<Object>> handlingAppException(AppException exception) {
+        ErrorCode errorCode = exception.getErrorCode();
+
+        return ResponseEntity.status(errorCode.getStatusCode()).body(
+                ApiResponse.builder()
+                        .code(errorCode.getCode())
+                        .message(errorCode.getMessage())
+                        .build()
+        );
+    }
+
+    @ExceptionHandler(value = AccessDeniedException.class)
+    ResponseEntity<ApiResponse<Object>> handlingAccessDeniedException(AccessDeniedException exception) {
+        ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
+
+        return ResponseEntity.status(errorCode.getStatusCode())
+                .body(ApiResponse.builder()
+                        .code(errorCode.getCode())
+                        .message(errorCode.getMessage())
+                        .build());
     }
 }
--- employer-service/src/main/java/com/recruitment/employer_service/exception/AppException.java
+++ employer-service/src/main/java/com/recruitment/employer_service/exception/AppException.java
@@ -1,5 +1,10 @@
 package com.recruitment.employer_service.exception;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class AppException extends RuntimeException {
 
     public AppException(ErrorCode errorCode) {
@@ -7,13 +12,6 @@ public class AppException extends RuntimeException {
         this.errorCode = errorCode;
     }
 
-    private ErrorCode errorCode;
+    private final ErrorCode errorCode;
 
-    public ErrorCode getErrorCode() {
-        return errorCode;
-    }
-
-    public void setErrorCode(ErrorCode errorCode) {
-        this.errorCode = errorCode;
-    }
 }
