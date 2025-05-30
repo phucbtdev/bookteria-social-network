9ed15d22240b51a7515ff8d6056fb74779a7b33e
Phúc Dev <phucbuitrong.dev@gmail.com>
2025-05-30 21:50:54 +0700

    Create event for kafka

--- job-service/src/main/java/com/recruitment/job_service/dto/event/JobDeletedEvent.java
+++ job-service/src/main/java/com/recruitment/job_service/dto/event/JobDeletedEvent.java
@@ -1,5 +1,14 @@
 package com.recruitment.job_service.dto.event;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.UUID;
 
-public record JobDeletedEvent(UUID id) {}
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JobDeletedEvent {
+    UUID id;
+}
