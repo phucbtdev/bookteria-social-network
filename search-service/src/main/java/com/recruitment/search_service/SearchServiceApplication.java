043a300989fb6357980865d21ce521dffd647297
Phúc Dev <phucbuitrong.dev@gmail.com>
2025-05-30 21:53:13 +0700

    Disable DataSourceAutoConfiguration.class & HibernateJpaAutoConfiguration.class

--- search-service/src/main/java/com/recruitment/search_service/SearchServiceApplication.java
+++ search-service/src/main/java/com/recruitment/search_service/SearchServiceApplication.java
@@ -2,9 +2,14 @@ package com.recruitment.search_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
 @EnableElasticsearchRepositories
 public class SearchServiceApplication {
 
