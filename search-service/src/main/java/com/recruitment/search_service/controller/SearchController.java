1d56492f40586e584ed7f4472f287dc3e4c1e5e3
Phúc Dev <phucbuitrong.dev@gmail.com>
2025-05-31 15:33:13 +0700

    Change prefix

--- search-service/src/main/java/com/recruitment/search_service/controller/SearchController.java
+++ search-service/src/main/java/com/recruitment/search_service/controller/SearchController.java
@@ -8,7 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/search")
+@RequestMapping("/search")
 @RequiredArgsConstructor
 public class SearchController {
 
