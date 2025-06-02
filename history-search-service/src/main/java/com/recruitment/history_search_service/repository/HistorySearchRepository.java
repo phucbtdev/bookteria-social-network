f8a02e0927161943d7d3714b98f101266188c425
Phúc Dev <phucbuitrong.dev@gmail.com>
2025-06-02 07:30:07 +0700

    Create repo

---
+++ history-search-service/src/main/java/com/recruitment/history_search_service/repository/HistorySearchRepository.java
@@ -0,0 +1,9 @@
+package com.recruitment.history_search_service.repository;
+
+import com.recruitment.history_search_service.entity.HistorySearch;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface HistorySearchRepository extends JpaRepository<HistorySearch, UUID> {
+}
