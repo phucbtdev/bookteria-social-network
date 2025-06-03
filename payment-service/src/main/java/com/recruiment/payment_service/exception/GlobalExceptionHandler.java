863de284dc0783bf5d70d1a40ae3490eb4d2bc83
Phúc Dev <phucbuitrong.dev@gmail.com>
2025-06-03 21:27:27 +0700

    Cấu hình exception

---
+++ payment-service/src/main/java/com/recruiment/payment_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,96 @@
+package com.recruiment.payment_service.exception;
+
+
+import com.recruitment.common.dto.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+@Slf4j
+public class
+GlobalExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    ResponseEntity<ApiResponse<Object>> handlingRuntimeException(RuntimeException exception) {
+        log.error("Exception: ", exception);
+
+        return ResponseEntity.badRequest()
+                .body(
+                        ApiResponse.builder()
+                                .code(ErrorCode.UNCATEGORIZED_EXCEPTION.getCode())
+                                .message(ErrorCode.UNCATEGORIZED_EXCEPTION.getMessage())
+                                .result(
+                                        null
+                                )
+                                .build()
+                );
+    }
+
+    @ExceptionHandler(value =  MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Map<String, String>>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        log.error("Validation error: ", ex);
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.badRequest()
+                .body(
+                        ApiResponse.<Map<String, String>>builder()
+                                .code(ErrorCode.UNCATEGORIZED_EXCEPTION.getCode())
+                                .result(errors)
+                                .build()
+                );
+    }
+
+    @ExceptionHandler(value = AppException.class)
+    ResponseEntity<ApiResponse<Object>> handlingAppException(AppException exception) {
+        ErrorCode errorCode = exception.getErrorCode();
+        log.error("Exception: ", exception);
+        return ResponseEntity.status(errorCode.getStatusCode()).body(
+                ApiResponse.builder()
+                        .code(errorCode.getCode())
+                        .message(errorCode.getMessage())
+                        .build()
+        );
+    }
+
+    @ExceptionHandler(value = AccessDeniedException.class)
+    ResponseEntity<ApiResponse<Object>> handlingAccessDeniedException(AccessDeniedException exception) {
+        ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
+        log.error("AccessDeniedException: ", exception);
+        return ResponseEntity.status(errorCode.getStatusCode())
+                .body(ApiResponse.builder()
+                        .code(errorCode.getCode())
+                        .message(errorCode.getMessage())
+                        .build());
+    }
+
+    @ExceptionHandler(value = HttpMessageNotReadableException.class)
+    ResponseEntity<ApiResponse<Object>> handlingAccessDeniedException(HttpMessageNotReadableException exception) {
+        ErrorCode errorCode = ErrorCode.REQUEST_JON_BODY_NOT_READABLE;
+        log.error("HttpMessageNotReadableException: ", exception);
+        return ResponseEntity.status(errorCode.getStatusCode())
+                .body(ApiResponse.builder()
+                        .code(errorCode.getCode())
+                        .message(exception.getMessage())
+                        .build());
+    }
+
+    @ExceptionHandler(value = MethodArgumentTypeMismatchException.class)
+    ResponseEntity<ApiResponse<Object>> handlingAccessDeniedException(MethodArgumentTypeMismatchException exception) {
+        ErrorCode errorCode = ErrorCode.METHOD_ARGUMENT_TYPE_MISMATCH;
+        log.error("MethodArgumentTypeMismatchException: ", exception);
+        return ResponseEntity.status(errorCode.getStatusCode())
+                .body(ApiResponse.builder()
+                        .code(errorCode.getCode())
+                        .message(exception.getMessage())
+                        .build());
+    }
+}
