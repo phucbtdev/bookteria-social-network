2518209eb7017ec23d84fd54a6a25d0a40d34c8f
Phúc Dev <phucbuitrong.dev@gmail.com>
2025-06-04 23:34:29 +0700

    Add oauth2-resource-server

--- payment-service/build.gradle.kts
+++ payment-service/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.5.0"
-	id("io.spring.dependency-management") version "1.1.7"
+	id("org.springframework.boot") version "3.2.5"
+	id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "com.recruiment"
@@ -36,6 +36,7 @@ dependencies {
 	implementation("com.recruitment:common:$commonVer")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.kafka:spring-kafka")
