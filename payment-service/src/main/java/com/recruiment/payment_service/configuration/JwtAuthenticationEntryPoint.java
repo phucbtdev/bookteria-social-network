37385e62b3da0b5a10009cc142270b46552cd356
Phúc Dev <phucbuitrong.dev@gmail.com>
2025-06-04 23:34:58 +0700

    Config security

---
+++ payment-service/src/main/java/com/recruiment/payment_service/configuration/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,34 @@
+package com.recruiment.payment_service.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.recruiment.payment_service.exception.ErrorCode;
+import com.recruitment.common.dto.response.ApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(
+            HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+            throws IOException {
+        ErrorCode errorCode = ErrorCode.UNAUTHENTICATED;
+
+        response.setStatus(errorCode.getStatusCode().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        ApiResponse<?> apiResponse = ApiResponse.builder()
+                .code(errorCode.getCode())
+                .message(errorCode.getMessage())
+                .build();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        response.getWriter().write(objectMapper.writeValueAsString(apiResponse));
+        response.flushBuffer();
+    }
+}
