12689c66df424268d6ac96aeb0eac9bcaa7cf357
Phúc Dev <phucbuitrong.dev@gmail.com>
2025-06-04 23:35:19 +0700

    Create repo

--- payment-service/src/main/java/com/recruiment/payment_service/repository/PaymentRepository.java
+++ payment-service/src/main/java/com/recruiment/payment_service/repository/PaymentRepository.java
@@ -2,9 +2,24 @@ package com.recruiment.payment_service.repository;
 
 import com.recruiment.payment_service.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
-    List<Payment> findByUserId(UUID userId);
+    List<Payment> findByUserIdAndUserType(UUID userId, Payment.UserType userType);
+
+    List<Payment> findByStatus(Payment.PaymentStatus status);
+
+    @Query("SELECT p FROM Payment p WHERE p.userId = :userId AND p.userType = :userType AND p.status = :status")
+    List<Payment> findByUserIdAndUserTypeAndStatus(
+            @Param("userId") UUID userId,
+            @Param("userType") Payment.UserType userType,
+            @Param("status") Payment.PaymentStatus status
+    );
+
+    Optional<Payment> findByPaymentId(UUID paymentId);
 }
