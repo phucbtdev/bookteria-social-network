7c33f89fadcad481772c0695e605492bba22e604
Phúc Dev <phucbuitrong.dev@gmail.com>
2025-06-05 14:20:02 +0700

    Thêm validation cho request

--- payment-service/src/main/java/com/recruiment/payment_service/dto/PaymentRequest.java
+++ payment-service/src/main/java/com/recruiment/payment_service/dto/PaymentRequest.java
@@ -1,8 +1,12 @@
 package com.recruiment.payment_service.dto;
 
 import com.recruiment.payment_service.entity.Payment;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.validator.constraints.URL;
 
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -13,12 +17,27 @@ import java.util.UUID;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class PaymentRequest {
+    @NotNull(message = "User ID is required")
     UUID userId;
+
+    @NotNull(message = "User type is required")
     Payment.UserType userType;
+
+    @NotNull(message = "Package ID is required")
     UUID packageId;
+
+    @NotNull(message = "Amount is required")
+    @DecimalMin(value = "0.01", inclusive = true, message = "Amount must be greater than 0")
     BigDecimal amount;
+
+    @NotNull(message = "Payment method is required")
+    @Pattern(regexp = "^(MOMO|VNPAY)$", message = "Invalid payment method")
     String paymentMethod;
+
+    @URL(message = "Invalid return URL")
     String returnUrl;
+
+    @URL(message = "Invalid notify URL")
     String notifyUrl;
 }
 
